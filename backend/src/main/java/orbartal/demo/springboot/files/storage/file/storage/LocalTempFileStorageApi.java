965059302508dc316f1aca948c2ff6a0e12c1818
Or Bartal <orbartal@gmail.com>
2023-05-04 22:03:47 -0400

    Add and use new spring config property file.storage.temp.dir

--- backend/src/main/java/orbartal/demo/springboot/files/storage/file/storage/LocalTempFileStorageApi.java
+++ backend/src/main/java/orbartal/demo/springboot/files/storage/file/storage/LocalTempFileStorageApi.java
@@ -7,6 +7,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -18,7 +19,8 @@ import orbartal.demo.springboot.files.storage.file.model.DownloadFileResult;
 @ConditionalOnProperty(value="file.storage.service",havingValue = "local.filesystem.temp")
 public class LocalTempFileStorageApi implements FileStorageApi {
 
-	private static String UPLOADED_FOLDER = "/tmp/uploads/";
+	@Value("${file.storage.temp.dir}")
+    private String filesDirPath;
 
 	private final DirValidator dirValidator;
 
@@ -28,22 +30,22 @@ public class LocalTempFileStorageApi implements FileStorageApi {
 
 	@PostConstruct
 	private void init() {
-		dirValidator.createIfNotExists(UPLOADED_FOLDER);
-		dirValidator.validateDirPath(UPLOADED_FOLDER);
+		dirValidator.createIfNotExists(filesDirPath);
+		dirValidator.validateDirPath(filesDirPath);
 	}
 
 	@Override
 	public String writeFile(MultipartFile file) throws IOException {
 		byte[] bytes = file.getBytes();
 		String fileName = file.getOriginalFilename();
-		Path path = Paths.get(UPLOADED_FOLDER + fileName);
+		Path path = Paths.get(filesDirPath + fileName);
 		Files.write(path, bytes);
 		return fileName;
 	}
 
 	@Override
 	public DownloadFileResult readFile(String fileName) throws IOException {
-		Path path = Paths.get(UPLOADED_FOLDER + fileName);
+		Path path = Paths.get(filesDirPath + fileName);
 		long fileSize = path.toFile().length();
 		InputStream body = new FileInputStream(path.toFile());
 		return new DownloadFileResult(fileName, body, fileSize);
