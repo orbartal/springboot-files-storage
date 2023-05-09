e0e6538935ffd9dbdca0207797eb73dfc74553c5
Or Bartal <orbartal@gmail.com>
2023-05-08 23:13:31 -0400

    Replace HadoopPropties in test with HadoopPropties in main code.

--- backend/src/test/java/orbartal/demo/springboot/files/storage/hadoop/config/HadoopProptiesProvider.java
+++ backend/src/test/java/orbartal/demo/springboot/files/storage/hadoop/config/HadoopProptiesProvider.java
@@ -1,5 +1,7 @@
 package orbartal.demo.springboot.files.storage.hadoop.config;
 
+import orbartal.demo.springboot.files.storage.hadoop.HadoopPropties;
+
 public class HadoopProptiesProvider {
 
 	private final static HadoopPropties propties = new HadoopPropties("http://namenode:9870", "/user/root/output/");
